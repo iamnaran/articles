13e492b18251762c66d66ccec660438fef991574
NaRan <nrn.panthi@gmail.com>
2021-04-29 14:29:00 +0545

    database fixes

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/app'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/app'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Mail
     MAIL_SERVER = 'smtp.googlemail.com'
